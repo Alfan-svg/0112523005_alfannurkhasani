cd3562a1d02c9f324ea00f1aed6df792ee154a95
Alfan Nurkhasani <alfannurhasani@gmail.com>
2026-06-27 11:07:22 +0700

    initial commit: sinkronisasi total dari lokal

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import app from "./app";
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Server Express.js berjalan",
+  });
+});
+app.get("/health", (req: Request, res: Response) => {
+  res.json({
+    message: "Server Express.js sehat",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
